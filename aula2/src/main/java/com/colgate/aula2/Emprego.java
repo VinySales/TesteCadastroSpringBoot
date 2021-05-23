5f9be66558f4f7f3e93600b3d76b369b0222db61
VinySales <vinysalesreboucas@hotmail.com>
2021-05-23 03:01:18 -0300

    aula2

---
+++ aula2/src/main/java/com/colgate/aula2/Emprego.java
@@ -0,0 +1,73 @@
+package com.colgate.aula2;
+
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+import org.hibernate.annotations.BatchSize;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+public class Emprego {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size (min = 4)
+    private String titulo;
+
+    @NotNull
+    @Size(min = 4)
+    private String empresa;
+
+    @NotNull
+    @Size(min = 10)
+    private String descricao;
+
+    private String data;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+}
